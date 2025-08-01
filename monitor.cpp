48b7a96ff7054b8023f41c7b68834a14f58e352b
Mohana <mohana.priya2@in.bosch.com>
2025-08-01 12:04:07 +0530

    Updated indent error 2

--- monitor.cpp
+++ monitor.cpp
@@ -5,45 +5,41 @@
 #include <iostream>
 #include <string>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
-void DisplayAlert (const std::string& message)
-{
-    cout << message;
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-}
-int IsTempOk(float temperature)
-  {
-     if (temperature > 102 || temperature < 95)
-     {
-     DisplayAlert("Temperature is critical!\n");
-    return 0;
-     }
-    return 1;
+
+void DisplayAlert(const std::string& message) {
+  cout << message;
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
   }
-int IsPulseOk(float pulseRate)
-  {
-    if (pulseRate < 60 || pulseRate > 100)
-    {
+}
+
+int IsTempOk(float temperature) {
+  if (temperature > 102 || temperature < 95) {
+    DisplayAlert("Temperature is critical!\n");
+    return 0;
+  }
+  return 1;
+}
+
+int IsPulseOk(float pulseRate) {
+  if (pulseRate < 60 || pulseRate > 100) {
     DisplayAlert("Pulse Rate is out of range!\n");
     return 0;
-    }
-    return 1;
   }
+  return 1;
+}
 
-int IsSpoOk(float spo2)
-  {
-   if (spo2 < 90)
-   {
+int IsSpoOk(float spo2) {
+  if (spo2 < 90) {
     DisplayAlert("Oxygen Saturation out of range!\n");
     return 0;
-   }
-    return 1;
   }
+  return 1;
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-return IsTempOk(temperature) && IsPulseOk(pulseRate) && IsSpoOk(spo2);
+  return IsTempOk(temperature) && IsPulseOk(pulseRate) && IsSpoOk(spo2);
 }
